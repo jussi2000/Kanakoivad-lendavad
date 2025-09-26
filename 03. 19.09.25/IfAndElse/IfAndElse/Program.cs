7d88b61e3667f6e63a72d2ffcb79912a15b9aaea
Opilane TTHK <opilane@tthk.ee>
2025-09-26 14:19:15 +0300

    IfAndElse

--- 03. 19.09.25/IfAndElse/IfAndElse/Program.cs
+++ 03. 19.09.25/IfAndElse/IfAndElse/Program.cs
@@ -12,29 +12,29 @@
             Console.WriteLine("peab käsitlema juhust, kus vastaja ei sisesta " +
                 "eelpool sisestatud värvi");
 
-            Console.WriteLine("Sisesta värv");
+            Console.WriteLine("Sisesta värv.");
 
             string color = Console.ReadLine();
 
             if (color == "red")
             {
-                Console.WriteLine("see on punane värv");
+                Console.WriteLine("see on punane värv.");
             }
             else if (color == "blue")
             {
-                Console.WriteLine("see on sinine värv");
+                Console.WriteLine("see on sinine värv.");
             }
             else if (color == "green")
             {
-                Console.WriteLine("see on roheline värv");
+                Console.WriteLine("see on roheline värv.");
             }
             else if (color == "white")
             {
-                Console.WriteLine("see on valge värv");
+                Console.WriteLine("see on valge värv.");
             }
             else
             {
-                Console.WriteLine("Sisestasid suvalise värvi");
+                Console.WriteLine("Sisestasid suvalise värvi.");
             }
         }
     }
